66ed036b9ec3da5581dd7ba7c9ce9e75ec9d862a
Brian Swan <bgswan@gmail.com>
2011-03-13 13:02:08 +0000

    Extracted method getAge.

--- src/com/codemanship/refactoring/assaultcourse/Customer.java
+++ src/com/codemanship/refactoring/assaultcourse/Customer.java
@@ -41,26 +41,7 @@ public class Customer {
 	public boolean isUnderAge(Rating rating) {
 
 		try {
-			// calculate customer's age in years and months
-			
-			// parse customer date of birth
-			Calendar calDateOfBirth = Calendar.getInstance();
-			calDateOfBirth.setTime(new SimpleDateFormat("yyyy-MM-dd").parse(getDateOfBirth()));
-			
-			// get current date
-			Calendar calNow = Calendar.getInstance();
-			calNow.setTime(new java.util.Date());  
-			
-			// calculate age different in years and months
-			int ageYr = (calNow.get(Calendar.YEAR) - calDateOfBirth.get(Calendar.YEAR));   
-			int ageMo = (calNow.get(Calendar.MONTH) - calDateOfBirth.get(Calendar.MONTH));
-			
-			// decrement age in years if month difference is negative
-			if (ageMo < 0)   
-			{   
-				ageYr--;   
-			}      
-			int age = ageYr;
+			int age = getAge();
 
 			// determine if customer is under legal age for rating
 			switch(rating){
@@ -80,5 +61,28 @@ public class Customer {
 		return false;		
 
 	}
-
+    
+    private int getAge() throws ParseException
+    {
+        // calculate customer's age in years and months
+		
+		// parse customer date of birth
+		Calendar calDateOfBirth = Calendar.getInstance();
+		calDateOfBirth.setTime(new SimpleDateFormat("yyyy-MM-dd").parse(getDateOfBirth()));
+		
+		// get current date
+		Calendar calNow = Calendar.getInstance();
+		calNow.setTime(new java.util.Date());  
+		
+		// calculate age different in years and months
+		int ageYr = (calNow.get(Calendar.YEAR) - calDateOfBirth.get(Calendar.YEAR));   
+		int ageMo = (calNow.get(Calendar.MONTH) - calDateOfBirth.get(Calendar.MONTH));
+		
+		// decrement age in years if month difference is negative
+		if (ageMo < 0)   
+		{   
+			ageYr--;   
+		}      
+		return ageYr;
+    }
 }
